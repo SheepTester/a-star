52f75ca2e0a1cfe2ff6f7d82957ed3700a2b0215
SheepTester <ussa.say@gmail.com>
2019-10-30 17:55:33 -0700

    add documentation to stack and implement queue (they've made pairs immutable??)

---
+++ queue.rkt
@@ -0,0 +1,58 @@
+#lang racket
+
+; (require rnrs/mutable-pairs-6)
+
+(define (make-queue)
+  ; pointers to first and last pair in queue (null when default)
+  (define first '())
+  (define last '())
+
+  ; whether or not the queue is empty
+  (define (empty?)
+    (null? first))
+
+  ; the number of items in the queue
+  (define (size)
+    ; there is no mlength :/
+    (define (iter mlist count)
+      (if (null? mlist) count
+          (iter (mcdr mlist) (+ count 1))))
+    (iter first 0))
+
+  ; adds item to end of queue
+  (define (add! item)
+    (if (empty?)
+        ; if empty, make a new list and make both pointer point to it
+        (let ((new-queue (mcons item '())))
+          (set! first new-queue)
+          (set! last new-queue))
+        ; else, make the cdr at the end of the queue hold a new pair
+        ; then point the last pair pointer to the new pair
+        (begin
+          (set-mcdr! last (mcons item '()))
+          (set! last (mcdr last)))))
+  
+  ; the item at front of queue
+  (define (peek)
+    (mcar first))
+
+  ; remove and return the item at front of queue
+  (define (remove!)
+    (let ((removed (peek)))
+      ; point first pair pointer to next pair
+      (set! first (mcdr first))
+      ; if first pair pointer is null, the queue must be empty,
+      ; so set last pair pointer to null too
+      (when (null? first)
+        (set! last '()))
+      removed))
+
+  ; allow methods to be accessed using (queue-instance 'method-name)
+  (define (dispatch method)
+    (cond ((equal? method 'empty?) empty?)
+          ((equal? method 'size) size)
+          ((equal? method 'add!) add!)
+          ((equal? method 'peek) peek)
+          ((equal? method 'remove!) remove!)
+          (else (error "Method doesn't exist"))))
+  dispatch)
--- stack.rkt
+++ stack.rkt
@@ -1,19 +1,32 @@
 #lang racket
 
 (define (make-stack)
+  ; store stack in list
   (define stack '())
+
+  ; whether or not the stack is empty
   (define (empty?)
     (null? stack))
+
+  ; the number of items in the stack
   (define (size)
     (length stack))
+
+  ; adds item to top of stack
   (define (add! item)
     (set! stack (cons item stack)))
+
+  ; the item on top of stack
   (define (peek)
     (car stack))
+
+  ; remove and return the item on top
   (define (remove!)
     (let ((removed (peek)))
       (set! stack (cdr stack))
       removed))
+
+  ; allow methods to be accessed using (stack-instance 'method-name)
   (define (dispatch method)
     (cond ((equal? method 'empty?) empty?)
           ((equal? method 'size) size)
