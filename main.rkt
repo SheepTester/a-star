a459a1c0af5012f0a6640db9f2046a0bf98ce74a
SheepTester <ussa.say@gmail.com>
2019-10-30 17:27:37 -0700

    implement stack and add GUI example

---
+++ main.rkt
@@ -0,0 +1,20 @@
+#lang racket/gui
+
+(require "./stack.rkt")
+
+; Make a frame by instantiating the frame% class
+(define frame (new frame% [label "Example"]))
+ 
+; Make a static text message in the frame
+(define msg (new message% [parent frame]
+                          [label "No events so far..."]))
+ 
+; Make a button in the frame
+(new button% [parent frame]
+             [label "Click Me"]
+             ; Callback procedure for a button click:
+             [callback (lambda (button event)
+                         (send msg set-label "Button click"))])
+ 
+; Show the frame by calling its show method
+(send frame show #t)
